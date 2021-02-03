b84834af6bcb2caf29244aaca5d217a21c077d9b
Bihan Jiang <jiang.bihan@gmail.com>
2021-02-03 13:20:36 -0800

    Add starter expo code with start screen

---
+++ stonks/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> Stonks </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+    color: '#fff'
+  }
+});
