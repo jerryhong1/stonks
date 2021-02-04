eed90a6f7f74f7f4e95ae081c29d220ad04caa46
Bihan Jiang <jiang.bihan@gmail.com>
2021-02-03 16:56:15 -0800

    Creating pages for home and login

---
+++ stonks/src/Home/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  text: {
+      fontFamily: 'Arial',
+      fontSize: 40,
+      color: '#fff'
+    },
+  Button: {
+    display: 'none',
+    color: '#fff',
+  }
+});
