e376df9df8ca6898dfd102d38f057537ceaa43ce
Tyler Packard <tyler.packard86@gmail.com>
2021-02-06 15:35:22 -0800

    Basic Firebase setup

--- stonks/App.js
+++ stonks/App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebaseTest from './src/firebase_test';
 
 export default function App() {
+  firebaseTest();
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}> Stonks </Text>
