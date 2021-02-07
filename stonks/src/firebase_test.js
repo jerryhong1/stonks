81e192a6759ca48253f15ee17f49ab7e73b67d29
Tyler Packard <tyler.packard86@gmail.com>
2021-02-07 14:48:36 -0800

    Add anonymous auth

--- stonks/src/firebase_test.js
+++ stonks/src/firebase_test.js
@@ -6,33 +6,60 @@ import 'firebase/firestore';
 // constant generated when first creating the project in Firebase.
 if (firebase.apps.length === 0) {
   const firebaseConfig = {
-    apiKey: "AIzaSyB386sF7xB0IXkG7KsWvFrdeYw6zXF4iBA",
-    authDomain: "stonks-cs194.firebaseapp.com",
-    projectId: "stonks-cs194",
-    storageBucket: "stonks-cs194.appspot.com",
-    messagingSenderId: "540190111052",
-    appId: "1:540190111052:web:dd4c5949229ca68223cbe9"
+    apiKey: 'AIzaSyB386sF7xB0IXkG7KsWvFrdeYw6zXF4iBA',
+    authDomain: 'stonks-cs194.firebaseapp.com',
+    projectId: 'stonks-cs194',
+    storageBucket: 'stonks-cs194.appspot.com',
+    messagingSenderId: '540190111052',
+    appId: '1:540190111052:web:dd4c5949229ca68223cbe9'
   };
   firebase.initializeApp(firebaseConfig);
 }
 
-export default async function firebaseTest() {
-  // Get the `test` document from the `users` collection.  Documents contain
-  // data and collections contain documents.  In this case, the `users`
-  // collection contains documents that store the data for an individual user.
-  const testDoc = firebase.firestore().collection('users').doc('test');
+async function firebaseTest() {
+  // Sign in to Firebase.  At some point we should enable users to create accounts
+  // so that they can sign in here.  For now we'll just create temporary anonymous
+  // accounts.
+  firebase.auth().signInAnonymously()
+    .then(() => {
+      // Signed in successfully.
+      console.log('Signed in anonymously as', firebase.auth().currentUser);
 
-  // Get a snapshot of the data in the `test` document from Firebase...
-  let testSnapshot = await testDoc.get();
-  let testData = testSnapshot.data();
+      // Get the `test` document from the `users` collection.  Documents contain
+      // data and collections contain documents.  In this case, the `users`
+      // collection contains documents that store the data for an individual user.
+      const testDoc = firebase.firestore().collection('users').doc('test');
 
-  // ...and modify and print out that data.
-  console.log('test has $' + testData.cash, 'in cash');
-  for (let stock of testData.stocks) {
-    stock.quantity++;
-    console.log('test owns', stock.quantity, 'shares of', stock.ticker);
-  }
+      // Get a snapshot of the data in the `test` document from Firebase...
+      return testDoc.get();
+    }, (error) => {
+      console.log('Sign in failed:', error);
+    })
+    .then((testSnapshot) => {
+      const testDoc = firebase.firestore().collection('users').doc('test');
+      let testData = testSnapshot.data();
 
-  // Save the modified data back in Firebase.
-  await testDoc.set(testData);
+      // ...and modify and print out that data.
+      console.log('test has $' + testData.cash, 'in cash');
+      for (let stock of testData.stocks) {
+        stock.quantity++;
+        console.log('test owns', stock.quantity, 'shares of', stock.ticker);
+      }
+
+      // Save the modified data back in Firebase.
+      return testDoc.set(testData);
+    }, (error) => {
+      console.log('Read failed:', error);
+    })
+    .then(() => {
+      console.log('Success!');
+    }, (error) => {
+      console.log('Write failed:', error);
+    })
+    .catch((error) => {
+      console.log(error);
+      return;
+    });
 }
+
+export default firebaseTest;
