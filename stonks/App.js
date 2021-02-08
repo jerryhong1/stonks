a3031967841fab009c3ccff34b7c0a629632b896
Lauren Yang <laurenyang@Laurens-MacBook-Pro.local>
2021-02-07 23:20:21 -0800

    added a new page for stock details and a placeholder chart with fake data

--- stonks/App.js
+++ stonks/App.js
@@ -1,3 +1,4 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
@@ -13,8 +14,51 @@ import LoginPage from "./App/Components/LoginPage";
 import TabScreen from "./App/Components/TabScreen";
 
 const Stack = createStackNavigator();
+import { VictoryLine, VictoryChart, VictoryTheme } from "victory-native";
 
-export default function App() {
+
+const data = [ //this is sample data for the sample chart on details
+  { x: 1, y: 13000 },
+  { x: 2, y: 16500 },
+  { x: 3, y: 14250 },
+  { x: 4, y: 19000 }
+];
+
+
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}> Stonks </Text>
+//       <StatusBar style="auto" />
+//       <Button
+//         title="Go to Details"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//     </View>
+//   );
+// }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> Test Details  </Text>
+      <VictoryChart theme={VictoryTheme.material}>
+      <VictoryLine 
+        width={350} 
+        style={{data: { stroke: "#c43a31" }}} 
+        theme={VictoryTheme.material} 
+        data={data} 
+        interpolation="natural"
+      />
+      </VictoryChart>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -25,10 +69,15 @@ export default function App() {
         <Stack.Screen name="Portfolio" component={Portfolio} />
         <Stack.Screen name="LoginPage" component={LoginPage} />
         <Stack.Screen name="TabScreen" component={TabScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
+  </NavigationContainer>
+    
   );
 }
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
