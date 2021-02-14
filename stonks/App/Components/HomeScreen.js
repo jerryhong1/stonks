382e911980cc58ab561aebf7499cac9c4d1f10e9
Tyler Packard <tyler.packard86@gmail.com>
2021-02-13 16:41:48 -0800

    Pull profile information from firebase

--- stonks/App/Components/HomeScreen.js
+++ stonks/App/Components/HomeScreen.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, ActivityIndicator} from 'react-native';
 import firebase from 'firebase';
 
