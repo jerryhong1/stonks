9a1c319c704e734c7721b6699137bb4dc318fa3d
Tyler Packard <tyler.packard86@gmail.com>
2021-03-06 16:58:04 -0800

    Move to library and clean up

--- stonks/App/Screens/HomeScreen.js
+++ stonks/App/Screens/HomeScreen.js
@@ -4,45 +4,9 @@ import { StyleSheet, Text, View, Button, ActivityIndicator} from 'react-native';
 import firebase from 'firebase';
 
 import {firebaseInit} from '../Lib/Firebase';
+import {stockUpdater} from '../Lib/StockUpdater';
 import Login from './Login';
 import Buttons from '../Styles/Buttons';
-
-// Update stock data on Firestore
-function stockUpdate() {
-  const globalStocksDoc = firebase.firestore().collection('global').doc('stocks');
-
-  try {
-    // Contend for right to pull data from polygon. Whoever succeeds in
-    // committing a successful transaction to Firestore first wins.
-    firebase.firestore().runTransaction(async (t) => {
-      const doc = await t.get(globalStocksDoc);
-      const polygonDelegate = doc.get('delegate'); // Debug: can remove this
-      const remoteTimestamp = doc.get('update_timestamp');
-      const localTimestamp = Math.floor(Date.now() / 60000);
-
-      // Debug: can remove this
-      console.log(`UID = "${polygonDelegate}"; LOCAL = ${localTimestamp}; REMOTE = ${remoteTimestamp}`);
-
-      // Update timestamp if no other user has already updated it
-      if (remoteTimestamp < localTimestamp) {
-        t.update(globalStocksDoc, {
-          delegate: firebase.auth().currentUser.uid,
-          update_timestamp: localTimestamp
-        });
-      } else {
-        // Fail if another user updated the timestamp before us
-        throw 'too slow!';
-      }
-    }).then(() => {
-      console.log('Transaction success!');
-      // TODO: make polygon API call here
-    }).catch((error) => {
-      console.log(error);
-    });
-  } catch (e) {
-    console.log(`Transaction failure: ${e}`);
-  }
-}
 
 export default function HomeScreen({navigation}) {
   const [shouldShow, setShouldShow] = useState(false);
@@ -69,30 +33,8 @@ export default function HomeScreen({navigation}) {
     });
 
     // Update stock data once per minute
-    const updateInterval = 60000; // 1 minute
-    stockUpdate();
-    let type = 'timeout';
-    let interval = 0;
-
-    const timeout = setTimeout(() => {
-      type = 'interval';
-
-      interval = setInterval(() => {
-        stockUpdate();
-      }, updateInterval);
-
-      stockUpdate();
-    }, updateInterval - (Date.now() % updateInterval));
-
-    return () => {
-      if (type === 'timeout') {
-        clearTimeout(timeout);
-      } else if (type === 'interval') {
-        clearInterval(interval);
-      } else {
-        console.error('uh oh');
-      }
-    }
+    const unsub = stockUpdater();
+    return unsub;
   }, []);
 
   return (
