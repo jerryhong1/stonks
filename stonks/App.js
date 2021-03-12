35f5055ba8821385139f63dd11e515ba212e39b9
Bihan Jiang <jiang.bihan@gmail.com>
2021-03-11 23:52:48 -0800

    Add new education screens to App.js

--- stonks/App.js
+++ stonks/App.js
@@ -13,6 +13,9 @@ import TabScreen from './App/Screens/TabScreen';
 import StockDetail from './App/Screens/StockDetail';
 import BuySellModal from './App/Screens/BuySell';
 import Education from './App/Screens/Education';
+import Investment from './App/Screens/EducationScreens/Investment';
+import Stock from './App/Screens/EducationScreens/Stock';
+import Market from './App/Screens/EducationScreens/Market';
 
 const RootStack = createStackNavigator();
 const MainStack = createStackNavigator();
@@ -29,6 +32,10 @@ function MainStackApp() {
       <MainStack.Screen name='StockDetail' component={StockDetail} />
       <MainStack.Screen name="BuySell" component={BuySellModal} options={{ headerShown: false }} mode="modal"/>
       <MainStack.Screen name="Education" component={Education} />
+      <MainStack.Screen name="Investment" component={Investment} />
+      <MainStack.Screen name="Stock" component={Stock} />
+      <MainStack.Screen name="Market" component={Market} />
+
     </MainStack.Navigator>
   );
 }
