fef57691ace819038c078ea3caa43093a037e5f2
Tyler Packard <tyler.packard86@gmail.com>
2021-03-14 11:46:12 -0700

    Add subscription to stock cache

--- stonks/App/Lib/StockCache.js
+++ stonks/App/Lib/StockCache.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
+// Yeah I know this should probably be a singleton or whatever, but I'm tired
 let stockCache = {};
+let subscribes = {null: []};
 let initialized = false;
 
-function initStockCache(callback) {
+function initStockCache(initCallback) {
   initialized = false;
 
   const unsub = firebase.firestore().collection('stocks').onSnapshot(snapshot => {
@@ -14,16 +16,32 @@ function initStockCache(callback) {
       // Cache data
       for (const doc of snapshot.docs) {
         stockCache[doc.id] = doc.data();
+        subscribes[doc.id] = [];
       }
 
-      // Make callback once cache is loaded for the first time
-      callback();
+      // Make init callback once cache is loaded for the first time
+      initCallback();
     } else {
       for (const change of snapshot.docChanges()) {
+        const doc = change.doc;
+
         if (change.type === 'removed') {
-          delete stockCache[change.doc.id];
+          delete stockCache[doc.id];
+          delete subscribes[doc.id]; // Maybe this shouldn't happen silently?
         } else {
-          stockCache[change.doc.id] = change.doc.data();
+          stockCache[doc.id] = doc.data();
+
+          // Make subscription callbacks
+          for (const subCallback of subscribes[null]) {
+            subCallback(doc);
+          }
+          if (subscribeStockCache.hasOwnProperty(doc.id)) {
+            for (const subCallback of subscribes[doc.id]) {
+              subCallback(doc);
+            }
+          } else {
+            subscribes[doc.id] = [];
+          }
         }
       }
     }
@@ -32,4 +50,24 @@ function initStockCache(callback) {
   return unsub;
 }
 
-export {stockCache, initStockCache};
+// Subscribe to updates for all tickers or a single ticker, if provided.
+// Returns an unsubscription callback.
+function subscribeStockCache(callback, ticker = null) {
+  if (!initialized) {
+    return null;
+  }
+
+  subscribes[ticker].push(callback);
+
+  // Unsubscription callback
+  return function() {
+    if (subscribes.hasOwnProperty(ticker)) {
+      let callbackIndex = subscribes.indexOf(callback);
+      if (callbackIndex !== -1) {
+        subscribes[ticker].splice(callbackIndex, 1);
+      }
+    }
+  };
+}
+
+export {stockCache, initStockCache, subscribeStockCache};
--- stonks/App/Screens/StockDetail.js
+++ stonks/App/Screens/StockDetail.js
@@ -171,29 +171,6 @@ export default function DetailsScreen({route, navigation}) {
     );
   };
 
-  /* a function to pull data from polygon and upload to firebase 
-  note you manually enter utcstart and utc end 
-  make sure you are uploading the correct desired granularity
-  press upload button to begin upload 
-  :D 
-  */
-  async function uploadData() {
-    var utcStart = new Date("2021-03-01"); //manually update this 
-    var utcEnd = new Date("2021-03-10"); //manually update this 
-    //https://api.polygon.io/v2/aggs/ticker/AAPL/range/5/minute/2020-10-14/2020-10-14?unadjusted=true&sort=asc&limit=5000&apiKey=VfpjQL3hxlS56WBVpmcslVQ5jCwm7U2m
-    var fullCall = URL + "ticker/" + stockData.ticker + "/range/30/minute/" + "2021-03-01" + "/" + "2021-03-12" + "?unadjusted=true&sort=asc&limit=5000&apiKey=" + KEY;
-    let response = await fetch(fullCall);
-    let data = await response.json();
-    var toUpload = { 
-      results: []
-    };
-    toUpload.results = data.results;
-    console.log('toUpload: ', toUpload);
-    console.log('pulled stock');
-    await firebase.firestore().collection('stocks').doc(stockData.ticker).set(toUpload, {merge: true});
-    console.log("success uploading", stockData.ticker, "data to firestore");
-  }
-
   function displayArticles() {
     return (
       <View style={{marginBottom: 200}}>
